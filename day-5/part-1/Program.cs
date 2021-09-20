5f264e841de3d7e8f85fed362e8d3643f2b8d4a0
Thea Bautista <thea.bautista00@gmail.com>
2021-09-20 17:21:36 +0100

    adding day 5 part 1

---
+++ day-5/part-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // open file
+            StreamReader file = new StreamReader(args[0]);
+            
+            uint highest_seat_ID = 0;
+
+            string line;
+            while ((line = file.ReadLine()) != null){
+                
+            // instantiate base product value
+            // read line by line
+                // record left and right pointer positions
+                // record back and front pointer positions
+                // for loop for each character
+                    // switch statement for back and front
+                    // switch statement for left and right
+                // multiply row and column
+                // keep product if it is higher than the last
+
+            
+            }
+            // return highest seat ID
+        }
+    }
+}
